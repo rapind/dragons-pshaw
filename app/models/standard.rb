5b950550f93a871c38768a66f6150e267d622e20
Dave Rapin <dave@rapin.com>
2016-03-01 13:33:24 -0500

    Fixed order clause on association

--- app/models/standard.rb
+++ app/models/standard.rb
@@ -1,6 +1,6 @@
 class Standard < ActiveRecord::Base
   belongs_to :strand
-  has_many :questions, -> { order: { difficulty: :desc } }
+  has_many :questions, -> { order(difficulty: :desc) }
 
   validates :name, presence: true
 end
